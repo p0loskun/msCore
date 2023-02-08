461ed8079ab3397c539a566a32a28d113402b9ee
Poloskun_ <nikongricisin339@gmail.com>
2023-02-08 21:01:10 +0300

    DateTimeFormatter

--- src/main/java/com/github/minersstudios/mscore/config/ConfigCache.java
+++ src/main/java/com/github/minersstudios/mscore/config/ConfigCache.java
@@ -2,19 +2,21 @@ package com.github.minersstudios.mscore.config;
 
 import com.github.minersstudios.mscore.MSCore;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.time.format.DateTimeFormatter;
 
 public class ConfigCache {
 	public final File dataFile;
 	public final YamlConfiguration yamlConfiguration;
 
-	public final String dateFormat;
+	public final @NotNull DateTimeFormatter timeFormatter;
 
 	public ConfigCache() {
 		this.dataFile = new File(MSCore.getInstance().getDataFolder(), "config.yml");
 		this.yamlConfiguration = YamlConfiguration.loadConfiguration(this.dataFile);
 
-		this.dateFormat = this.yamlConfiguration.getString("date-format");
+		this.timeFormatter = DateTimeFormatter.ofPattern(this.yamlConfiguration.getString("date-format", "EEE, yyyy-MM-dd HH:mm z"));
 	}
 }
