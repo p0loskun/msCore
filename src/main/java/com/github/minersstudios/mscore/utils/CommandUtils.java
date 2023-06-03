5062bf24e8240852bcecf268aec1ecd130c2de07
Poloskun_ <nikongricisin339@gmail.com>
2023-06-03 18:49:11 +0300

    now its too cool

--- src/main/java/com/github/minersstudios/mscore/utils/CommandUtils.java
+++ src/main/java/com/github/minersstudios/mscore/utils/CommandUtils.java
@@ -4,6 +4,7 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.time.DateTimeException;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
@@ -46,20 +47,24 @@ public final class CommandUtils {
 	 * @param string time
 	 * @return date with time added
 	 */
-	public static @Nullable Date getDateFromString(@NotNull String string) throws NumberFormatException {
+	public static @Nullable Date getDateFromString(@NotNull String string) throws NumberFormatException, DateTimeException, ArithmeticException {
 		if (!string.matches("\\d+[smhdMy]")) return null;
-		long number = Long.parseLong(string.replaceAll("[smhdMy]", ""));
 		String chronoUnit = string.replaceAll("\\d+", "");
 		Instant instant = Instant.now();
-		return Date.from(
-				switch (chronoUnit) {
-					case "s" -> instant.plus(number, ChronoUnit.SECONDS);
-					case "m" -> instant.plus(number, ChronoUnit.MINUTES);
-					case "h" -> instant.plus(number, ChronoUnit.HOURS);
-					case "M" -> instant.plus(Math.multiplyExact(number, ChronoUnit.MONTHS.getDuration().toDays()), ChronoUnit.DAYS);
-					case "y" -> instant.plus(Math.multiplyExact(number, ChronoUnit.YEARS.getDuration().toDays()), ChronoUnit.DAYS);
-					default -> instant.plus(number, ChronoUnit.DAYS);
-				}
-		);
+		try {
+			long number = Long.parseLong(string.replaceAll("[smhdMy]", ""));
+			return Date.from(
+					switch (chronoUnit) {
+						case "s" -> instant.plus(number, ChronoUnit.SECONDS);
+						case "m" -> instant.plus(number, ChronoUnit.MINUTES);
+						case "h" -> instant.plus(number, ChronoUnit.HOURS);
+						case "M" -> instant.plus(Math.multiplyExact(number, ChronoUnit.MONTHS.getDuration().toDays()), ChronoUnit.DAYS);
+						case "y" -> instant.plus(Math.multiplyExact(number, ChronoUnit.YEARS.getDuration().toDays()), ChronoUnit.DAYS);
+						default -> instant.plus(number, ChronoUnit.DAYS);
+					}
+			);
+		} catch (DateTimeException | NumberFormatException | ArithmeticException ignored) {
+			return null;
+		}
 	}
 }
