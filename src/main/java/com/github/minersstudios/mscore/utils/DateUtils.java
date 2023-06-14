e2a0344e03fbd6fa80bd588248f17816954ab8a2
Poloskun_ <nikongricisin339@gmail.com>
2023-06-14 21:45:46 +0300

    moved CommandUtils to DateUtils

--- src/main/java/com/github/minersstudios/mscore/utils/DateUtils.java
+++ src/main/java/com/github/minersstudios/mscore/utils/DateUtils.java
@@ -1,7 +1,6 @@
 package com.github.minersstudios.mscore.utils;
 
 import com.github.minersstudios.mscore.MSCore;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
@@ -14,13 +13,18 @@ import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.URL;
+import java.time.DateTimeException;
 import java.time.Instant;
 import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.logging.Level;
 
 @SuppressWarnings("unused")
 public final class DateUtils {
+	public static final String CHRONO_REGEX = "\\d+[smhdMy]";
 
 	@Contract(value = " -> fail")
 	private DateUtils() {
@@ -54,24 +58,6 @@ public final class DateUtils {
 	}
 
 	/**
-	 * Gets date with sender time zone
-	 *
-	 * @param date   date to be converted
-	 * @param sender sender (can be player)
-	 * @return string date format
-	 */
-	public static @NotNull String getSenderDate(
-			@NotNull Date date,
-			@NotNull CommandSender sender
-	) {
-		if (sender instanceof Player player) {
-			InetSocketAddress socketAddress = player.getAddress();
-			return getDate(date, socketAddress != null ? socketAddress.getAddress() : null);
-		}
-		return getDate(date, null);
-	}
-
-	/**
 	 * Gets timezone from ip
 	 *
 	 * @param ip IP address
@@ -98,5 +84,102 @@ public final class DateUtils {
 			MSCore.getInstance().getLogger().log(Level.WARNING, e.getMessage());
 			return ZoneId.systemDefault().toString();
 		}
+	}
+
+	/**
+	 * Gets date with player time zone
+	 *
+	 * @param date   date to be converted
+	 * @param player player
+	 * @return string date format
+	 */
+	public static @NotNull String getPlayerDate(
+			@NotNull Date date,
+			@Nullable Player player
+	) {
+		if (player != null) {
+			InetSocketAddress socketAddress = player.getAddress();
+			return DateUtils.getDate(date, socketAddress != null ? socketAddress.getAddress() : null);
+		}
+		return DateUtils.getDate(date, null);
+	}
+
+	/**
+	 * Gets time suggestions from number
+	 * <p>
+	 * Used for command tab completer
+	 *
+	 * @param input number of time
+	 * @return time suggestions
+	 */
+	public static @NotNull List<String> getTimeSuggestions(@NotNull String input) {
+		List<String> suggestions = new ArrayList<>();
+		if (!input.matches("\\d+")) return suggestions;
+		suggestions.add(input + "s");
+		suggestions.add(input + "m");
+		suggestions.add(input + "h");
+		suggestions.add(input + "d");
+		suggestions.add(input + "M");
+		suggestions.add(input + "y");
+		return suggestions;
+	}
+
+	/**
+	 * Gets a date with time added
+	 * <p>
+	 * Regex : \d+[smhdMy]
+	 *
+	 * @param string         time
+	 * @param throwException if true, an exception will be thrown
+	 * @return date with time added
+	 * @throws NumberFormatException if the string does not contain a parsable long
+	 * @throws DateTimeException     if the chrono unit value is too big and the addition cannot be made
+	 * @throws ArithmeticException   if numeric overflow occurs
+	 */
+	public static @Nullable Date getDateFromString(
+			@NotNull String string,
+			boolean throwException
+	) throws NumberFormatException, DateTimeException, ArithmeticException {
+		if (!string.matches(CHRONO_REGEX)) return null;
+
+		String chronoUnit = string.replaceAll("\\d+", "");
+		Instant instant = Instant.now();
+
+		try {
+			long number = Long.parseLong(string.replaceAll("[smhdMy]", ""));
+			return Date.from(
+					switch (chronoUnit) {
+						case "s" -> instant.plus(number, ChronoUnit.SECONDS);
+						case "m" -> instant.plus(number, ChronoUnit.MINUTES);
+						case "h" -> instant.plus(number, ChronoUnit.HOURS);
+						case "M" -> instant.plus(Math.multiplyExact(number, ChronoUnit.MONTHS.getDuration().toDays()), ChronoUnit.DAYS);
+						case "y" -> instant.plus(Math.multiplyExact(number, ChronoUnit.YEARS.getDuration().toDays()), ChronoUnit.DAYS);
+						default -> instant.plus(number, ChronoUnit.DAYS);
+					}
+			);
+		} catch (DateTimeException | NumberFormatException | ArithmeticException e) {
+			if (throwException) throw e;
+			return null;
+		}
+	}
+
+	/**
+	 * Gets a date with time added
+	 * <p>
+	 * Regex : \d+[smhdMy]
+	 *
+	 * @param string time
+	 * @return date with time added
+	 * @throws NumberFormatException if the string does not contain a parsable long
+	 * @throws DateTimeException     if the chrono unit value is too big and the addition cannot be made
+	 * @throws ArithmeticException   if numeric overflow occurs
+	 */
+	public static @Nullable Date getDateFromString(@NotNull String string) throws NumberFormatException, DateTimeException, ArithmeticException {
+		return getDateFromString(string, true);
+	}
+
+	@Contract(value = "null -> false", pure = true)
+	public static boolean matchesChrono(@Nullable String string) {
+		return string != null && string.matches(CHRONO_REGEX);
 	}
 }
